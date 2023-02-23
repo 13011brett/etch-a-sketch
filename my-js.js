cb65a97f748f6fd818531eff7bacf5a8f1ab6203
13011brett <scarlett.brett123@gmail.com>
2023-02-22 20:46:56 -0500

    Accidentally didn't stage other changes..

--- my-js.js
+++ my-js.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 console.log(container);
 
 
-for(var i = 0; i < 246; i++){
+for(var i = 0; i < 256; i++){
     var divs = document.createElement('div');
     divs.classList.add('sub');
     container.appendChild(divs);
