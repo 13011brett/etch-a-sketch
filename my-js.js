cdd0dbef0861b6644ff3cf331b79cb0483f7162a
13011brett <scarlett.brett123@gmail.com>
2023-02-22 23:47:39 -0500

    Reset now is functional.

--- my-js.js
+++ my-js.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-console.log(container);
+const reset = document.querySelector(".reset-etch");
 
 // Currently creating a static 256 amount based on TheOdinProject requirements. (could make it better by doing 16, then starting a new div)
 for(var i = 0; i < 256; i++){
@@ -11,6 +11,11 @@ for(var i = 0; i < 256; i++){
 const etchBoxes = document.querySelectorAll('.sub');
 
 // Loop through the node list and listen for mouse over events.
+
+reset.addEventListener('click', () => {
+    etchBoxes.forEach(e => e.style.backgroundColor = '')
+})
+
 etchBoxes.forEach((e) => e.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = 'black';
 })
