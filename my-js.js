d0f5a6b13a5dd6b68b602f26f67560cd31c61e2c
13011brett <scarlett.brett123@gmail.com>
2023-02-22 19:14:52 -0500

    Some sort of start to divs being generated w/ flex

--- my-js.js
+++ my-js.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 console.log(container);
 
 
-for(var i = 0; i < 10; i++){
+for(var i = 0; i < 246; i++){
     var divs = document.createElement('div');
     divs.classList.add('sub');
     container.appendChild(divs);
